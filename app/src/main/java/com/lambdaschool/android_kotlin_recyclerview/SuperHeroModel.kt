cc7bcd44beca76b490ccbc1adbfc78e9ac7eae59
heiligbasil <basilief@yahoo.com>
2019-05-29 19:33:23 -0700

    Excluded superfluous class model initializers

--- app/src/main/java/com/lambdaschool/android_kotlin_recyclerview/SuperHeroModel.kt
+++ app/src/main/java/com/lambdaschool/android_kotlin_recyclerview/SuperHeroModel.kt
@@ -6,9 +6,9 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class SuperHero(
-    val response: String? = "",
-    val id: String? = "",
-    val name: String? = "",
+    val response: String?,
+    val id: String?,
+    val name: String?,
     val powerstats: Powerstats?,
     val biography: Biography?,
     val appearance: Appearance?,
@@ -19,58 +19,58 @@ data class SuperHero(
 
 @Serializable
 data class Base(
-    @SerialName("results-for") val results_for: String? = "",
+    @SerialName("results-for") val results_for: String?,
     val results: List<SuperHero>?
 )
 
 @Serializable
 data class Powerstats(
-    val intelligence: String? = "",
-    val strength: String? = "",
-    val speed: String? = "",
-    val durability: String? = "",
-    val power: String? = "",
-    val combat: String? = ""
+    val intelligence: String?,
+    val strength: String?,
+    val speed: String?,
+    val durability: String?,
+    val power: String?,
+    val combat: String?
 )
 
 @Serializable
 data class Biography(
-    @SerialName("full-name") val full_name: String? = "",
-    @SerialName("alter-egos") val alter_egos: String? = "",
+    @SerialName("full-name") val full_name: String?,
+    @SerialName("alter-egos") val alter_egos: String?,
     val aliases: List<String>?,
     @SerialName(
         "place-of-birth"
-    ) val place_of_birth: String? = "",
+    ) val place_of_birth: String?,
     @SerialName(
         "first-appearance"
-    ) val first_appearance: String? = "",
-    val publisher: String? = "",
-    val alignment: String? = ""
+    ) val first_appearance: String?,
+    val publisher: String?,
+    val alignment: String?
 )
 
 @Serializable
 data class Appearance(
-    val gender: String? = "",
-    val race: String? = "",
+    val gender: String?,
+    val race: String?,
     val height: List<String>?,
     val weight: List<String>?,
-    @SerialName("eye-color") val eye_color: String? = "",
-    @SerialName("hair-color") val hair_color: String? = ""
+    @SerialName("eye-color") val eye_color: String?,
+    @SerialName("hair-color") val hair_color: String?
 )
 
 @Serializable
 data class Work(
-    val occupation: String? = "",
-    val base: String? = ""
+    val occupation: String?,
+    val base: String?
 )
 
 @Serializable
 data class Connections(
-    @SerialName("group-affiliation") val group_affiliation: String? = "",
-    val relatives: String? = ""
+    @SerialName("group-affiliation") val group_affiliation: String?,
+    val relatives: String?
 )
 
 @Serializable
 data class Image(
-    val url: String? = ""
+    val url: String?
 )
